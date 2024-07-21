e316f4bc1534f83e92e7fd95a9821d50b753ba97
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-21 14:32:42 +0200

    feat(NotFound): replaced direct relative path with an reference, not necessary here but this can solve issues where relative paths are not taken into account in other scenarios

--- client/src/pages/NotFoundPage/NotFound.jsx
+++ client/src/pages/NotFoundPage/NotFound.jsx
@@ -1,15 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "./NotFound.css";
+import notFound from "../../../static/images/notfound.png";
 
 export default function NotFound() {
   return (
     <div className="notFound">
       <h1 className="errorTitle">Erreur 404, page non trouvée</h1>
-      <img
-        src="../../../static/images/notfound.png"
-        alt=""
-        className="errorImage"
-      />
+      <img src={notFound} alt="" className="errorImage" />
       <div className="errorContainer">
         <p className="errorMessage">Vos vacances, ne se trouvent pas ici.</p>
         <div className="redirectContainer">
